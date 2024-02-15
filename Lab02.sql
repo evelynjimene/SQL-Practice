dbc225df8daa64600369a69c18cd7043e917cd51
Evelyn Jimenez <159857814+evelynjimene@users.noreply.github.com>
2024-02-14 21:24:40 -0800

    Add files via upload

---
+++ Lab02.sql
@@ -0,0 +1,12 @@
+USE AdventureWorks2012
+
+SELECT ProductID, Name, SellStartDate
+FROM Production.Product
+/* 504 rows were returned */
+
+USE SchoolSchedulingExample
+
+SELECT StudFirstName, StudLastName, StudState, StudPhoneNumber
+FROM dbo.Students
+/* 18 rows were returned */
+
